ef78d89c8cb893e755328f183050845d72d66bc6
Owen Anderchek <62686065+king-owen@users.noreply.github.com>
2022-02-18 14:05:10 -0800

    Updating a lot

---
+++ acit_3495_project/upload_video/app.py
@@ -0,0 +1,32 @@
+import os
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+uploads = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'upload_folder')
+#print(os.path.dirname(os.path.realpath(__file__)))
+print(uploads)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/upload')
+def upload_files():
+    return render_template('upload.html')
+
+@app.route('/uploader', methods = ['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        f = request.files['file']
+        #f.save(f.filename)
+        #print(f)
+        f.save(os.path.join(uploads, f.filename))
+        
+        return 'successful upload'
+
+
+
+
+if __name__ == '__main__':
+    app.run()
