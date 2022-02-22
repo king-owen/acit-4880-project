452e0203ff1d0728ac4513a1f1f1cac4559bf5fe
Owen Anderchek <62686065+king-owen@users.noreply.github.com>
2022-02-21 20:03:04 -0800

    ftp folders

--- acit_3495_project/file_system/ftpserver.py
+++ acit_3495_project/file_system/ftpserver.py
@@ -17,8 +17,8 @@ FTP_PASSWORD = "password"
 print(os.getcwd())
 # The directory the FTP user will have full read/write access to.
 #FTP_DIRECTORY = "/srv/users/SYSUSER/apps/APPNAME/public/"
-FTP_DIRECTORY = "/acit-4880-project/acit_3495/file_system/uploads"
-#FTP_DIRECTORY = "./uploads"
+#FTP_DIRECTORY = "/acit-4880-project/acit_3495/file_system/uploads"
+FTP_DIRECTORY = "./uploads"
 
 
 
