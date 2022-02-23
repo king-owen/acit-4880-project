b9e7ccc672bb65ab43077e8c54df1a64f31b09d5
Owen Anderchek <62686065+king-owen@users.noreply.github.com>
2022-02-23 09:15:13 -0800

    Update app.py

--- acit_3495_project/video_streaming/app.py
+++ acit_3495_project/video_streaming/app.py
@@ -7,12 +7,12 @@ import json
 #import wget
 app = Flask(__name__, static_folder='static')
 
-#mydb = mysql.connector.connect(
-#    host='172.6.0.2',
-#    user='root',
-#    password='root',
-#    database='video'
-#)
+mydb = mysql.connector.connect(
+    host='172.6.0.2',
+    user='root',
+    password='root',
+    database='video'
+)
 
 #mycursor = mydb.cursor()
 
@@ -48,13 +48,6 @@ def hello_world():
 def download_files():
     if check_login() != True:
         return redirect("http://172.6.0.6:8081/login")
-    
-    mydb = mysql.connector.connect(
-        host='172.6.0.2',
-        user='root',
-        password='root',
-        database='video'
-    )
 
     mycursor = mydb.cursor()
 
