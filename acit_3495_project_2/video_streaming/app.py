137d02e6090286dfb822b7656772846516b5af45
Owen Anderchek <62686065+king-owen@users.noreply.github.com>
2022-04-02 21:58:07 -0700

    mysql

--- acit_3495_project_2/video_streaming/app.py
+++ acit_3495_project_2/video_streaming/app.py
@@ -8,7 +8,7 @@ import json
 app = Flask(__name__, static_folder='static')
 
 mydb = mysql.connector.connect(
-    host='172.68.0.2',
+    host='load-mysql',
     #port='3307',
     user='root',
     password='root',
